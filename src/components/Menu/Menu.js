7e6f2c5ca51eab3cf315e14238bd887cb57984de
pnkvnkt <chennikita70@gmail.com>
2023-06-15 21:48:26 +0500

    added 404 Error on a main screen

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductСard/ProductCard";
-import Sceleton from "./Sceleton";
+import Sceleton from "./Sceleton/Sceleton";
 import style from "./Menu.module.css";
 import Button from "../assets/Button/Button";
 
@@ -10,10 +10,8 @@ const Menu = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    console.log("start");
-    axios.get("http://185.246.64.64:4999/food/get").then((response) => {
+    axios.get("http://185.246.64.64:4999/content/feed").then((response) => {
       setProducts(response.data);
-      // setTimeout(() => setIsLoading(false), 5000)
       setIsLoading(false);
     });
   }, []);
@@ -25,19 +23,28 @@ const Menu = () => {
         <div className={style.productCardsWrapper}>
           {isLoading
             ? [...new Array(5)].map((item, index) => (
-                <Sceleton key={index} className={style.productCard} />
+                <div className={style.sceletonWrapper}>
+                  <Sceleton key={index} className={style.productCard} />
+                </div>
               ))
             : products.map((productData) => (
                 <ProductCard
                   key={productData.id}
-                  {...[productData]}
+                  {...productData}
                   className={style.productCard}
                 />
               ))}
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
+          {/* <ProductCard></ProductCard>
+          <ProductCard></ProductCard> */}
         </div>
-        <Button styleClass="moreButton" />
+        <Button styleClass="moreButton">
+          Больше блюд
+          <img
+            src={"./images/button_more-icon.svg"}
+            alt=""
+            className={style.moreIcon}
+          ></img>
+        </Button>
       </div>
     </section>
   );
--- src/components/Menu/ProductСard/ProductCard.js
+++ src/components/Menu/ProductСard/ProductCard.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
+import axios from "axios";
 import Button from "../../assets/Button/Button";
 import style from "./ProductCard.module.css";
 
-const ProductCard = () => {
+
+const ProductCard = props => {
+  console.log('http://185.246.64.64:4999/images/' + props.image)
+  useEffect(() => {
+    axios.get('http://185.246.64.64:4999/images/' + props.image, {reponseType:'blob'}).then((response) => {
+      console.log('cool')
+    });
+  }, []);
+
   return (
     <article className={style.productCard}>
       <div className={style.imgWrapper}></div>
       <div className={style.wrapper}>
         <div className={style.textWrapperLeft}>
-          <p className={style.name}>Лагман </p>
-          <p className={style.price}>290₽</p>
+          <p className={style.name}>{props.name}</p>
+          <p className={style.price}>{`${props.price}₽`}</p>
         </div>
-        <div className={style.textWrapperRight}>810 г</div>
+        <div className={style.textWrapperRight}>{`${props.mass} г`}</div>
         <Button styleClass = 'addButton'>Добавить</Button>
       </div>
     </article>
