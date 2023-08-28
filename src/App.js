59f610a893c4d68dde96565b4758aca09d950745
pnkv <chennikita70@gmail.com>
2023-08-28 21:45:06 +0500

    add react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header.js";
 import Feed from "./components/Feed/Feed";
 import Info from "./components/Info/Info";
-// import Menu from "./components/Menu/Menu";
-// import CartPopUp from "./components/CartPopUp/CartPopUp";
+import Menu from "./components/Menu/Menu";
 
 function App() {
   return (
     <div>
       <Header />
-      <Feed />
+      <Routes>
+        <Route path="" element={<Feed />} />
+        <Route path="/menu" element={<Menu/>} />
+        <Route path="*" element={<h1>not found</h1>} />
+      </Routes>
       <Info />
+
       {/* <CartPopUp/> */}
     </div>
   );
--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -1,12 +1,33 @@
-import React from "react";
-import style from './Nav.module.css'
+import React, { useEffect, useState } from "react";
+import style from "./Nav.module.css";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const active = style.activePage;
+  const [activeLink, setActiveLink] = useState("Главная");
   return (
     <nav className={style.nav}>
-      <a className={style.activePage} href="##">Главная</a>
-      <a href="##">Меню</a>
-      <a href="##">Контакты</a>
+      <Link
+        className={activeLink === "Главная" ? active : ""}
+        onClick={() => setActiveLink("Главная")}
+        to="/"
+      >
+        Главная
+      </Link>
+      <Link
+        className={activeLink === "Меню" ? active : ""}
+        onClick={() => setActiveLink("Меню")}
+        to="/menu"
+      >
+        Меню
+      </Link>
+      <Link
+        className={activeLink === "Контакты" ? active : ""}
+        onClick={() => setActiveLink("Контакты")}
+        to="/contact"
+      >
+        Контакты
+      </Link>
     </nav>
   );
 };
